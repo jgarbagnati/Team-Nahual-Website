601e17d3852642c15181b78d3fdc5f33c4f0dc9f
CarpeSwag <james@garbagnati.com>
2018-03-17 05:16:00 -0700

    Added more content to the main page.

--- src/components/MainContents.jsx
+++ src/components/MainContents.jsx
@@ -1,7 +1,49 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 
+const optionTitle = ['Story', 'Explore', 'Nahual'];
+const optionText = ["Juanito el Nahualito is a story that takes places in current time. A story about a teenager named Juanito. Juanito lives with his grandfather who migrated to the city from his village to take care of his grandson. Juanito Was given to his grandfather to take care of him as his Mother tries to provide from another city. Juanito grows up with his grandfather who used to tell stories to Juanito about his heritage and roots. As Juanito grows up to be a teen, he stops believing his grandfather about those stories. At the age of 16, Juanito has an internal conflict of who he is. Juan (the grandfather) falls ill and asks Juanito to help him. As Juanito tries to help his grandfather, his grandfather leaves mysteriously. Juanito then decides to follow him and loses him in the forest. Juanito falls and faints in the forest; this is where he discovers a power that his grandfather once told him as story/fable.", "The look and feel of the Juanito el Nahualito is a reference to wood carving that tells a story and Mesoamerican oral tradition of storytelling. Magical Realism is applied to tell a coming-of-age story.", "In Mesoamerican cultures like Aztecs, Mayans, Zapotecs, and Mixtecs, the shaman or priest was known as the nahual. According to tradition, the nahuales could transform into animals and penetrate with their soul the body of an animal or natural phenomena. By adopting these appearances, these nahuales performed their prodigious actions. The nahual is the animal that gives us its characteristics soon after our birth. It is told that we all are born with a nahual, which is the animal that is born at the same time as we are."];
+
 export default class MainContents extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			opt: 0
+		};
+		
+		this.explore = this.explore.bind(this);
+		this.nahual = this.nahual.bind(this);
+		this.story = this.story.bind(this);
+	}
+	
+	renderTitles() {
+		let fun = [this.story, this.explore, this.nahual];
+		let opts = [];
+		for (let i = 0; i < optionTitle.length; ++i) {
+			let className = (i == this.state.opt)? "selected": "";
+			opts.push(
+				<div onClick={fun[i]}
+					className={className}>
+					{optionTitle[i]}
+				</div>
+			);
+		}
+		
+		return opts;
+	}
+	
+	renderText() {
+		return optionText[this.state.opt];
+	}
+	
+	selectOption(opt) {
+		this.setState({opt: opt});
+	}
+	
+	story() {this.selectOption(0);}
+	explore() {this.selectOption(1);}
+	nahual() {this.selectOption(2);}
 	
 	render() {
 		return (
@@ -18,6 +60,20 @@ export default class MainContents extends Component {
 				</div>
 				
 				<div className='divider-bar' />
+				
+				<div className='text'>
+					<div className='options'>
+						{this.renderTitles()}
+					</div>
+					{this.renderText()}
+				</div>
+				
+				<div className='divider-bar' />
+				
+				<div className='platforms'>
+					Will be available to play on
+					<span> Windows • Mac • Linux </span>
+				</div>
 				
 				<div className='social-media-cntr'>
 					<div className='media-text'> Follow development at: </div>
