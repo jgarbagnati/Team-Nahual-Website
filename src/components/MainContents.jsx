ea1576bb416590d5f7adb1ee638c59eb7dfecbd3
CarpeSwag <james@garbagnati.com>
2018-03-17 00:34:14 -0700

    Updated game description.

--- src/components/MainContents.jsx
+++ src/components/MainContents.jsx
@@ -13,8 +13,8 @@ export default class MainContents extends Component {
 				
 				<div className='divider-bar' />
 				
-				<div className='text'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<div className='description text'>
+					Juanito el Nahualito is a beautiful mesoamerican adventure about a teenager who is in search of grandfather and learns about his culture. You play as Juanito, a Nahual capable of having a close connection with nature. Juanito discovers his powers as he remembers his grandfather stories about his Mesoamerican culture.
 				</div>
 				
 				<div className='divider-bar' />
