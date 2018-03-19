aaa6c7f040ac802557f948113bcadeda16adf340
CarpeSwag <james@garbagnati.com>
2018-03-19 05:07:36 -0700

    Parallax component now actually performs simple parallax effect.

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,13 +1,50 @@
 import {Component} from 'inferno';
 
 export default class Parallax extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			scrollTop: 0
+		};
+		
+		this.handleScroll = this.handleScroll.bind(this);
+	}
+	
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll);
+	}
+	
+	handleScroll(event) {
+		this.setState({
+			scrollTop: event.srcElement.body.scrollTop
+		});
+	}
 	
 	render() {
+		let height = 800 - this.state.scrollTop / 2;
+		let style = {
+			height: height
+		};
+		let vignetteStyle = {
+			height: height,
+			marginTop: -height
+		};
+		let gradientStyle = {
+			height: height / 2,
+			marginTop: -height / 2
+		};
 		return (
-			<div className='parallax-cntr'>
-				<div className='image' />
-				<div className='vignette' />
-				<div className='fade-in-gradient' />
+			<div className='parallax-cntr' style={style}>
+				<div className='image' style={style} />
+				<div className='vignette'
+					style={vignetteStyle} />
+				<div className='fade-in-gradient' 
+					style={gradientStyle}/>
 			</div>
 		);
 	}
